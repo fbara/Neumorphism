329f8a405bac18b0d789a29c490d76de6c0a35b4
Frank S. Bara <fbara1@yahoo.com>
2020-02-27 22:44:46 -0600

    Created rounded rectangle with white and black shadows.

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -8,9 +8,24 @@
 
 import SwiftUI
 
+extension Color {
+    // Slightly off white
+    static let offWhite = Color(red: 225/255, green: 225/255, blue: 235/255)
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color.offWhite
+            
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.offWhite)
+                .frame(width: 300, height: 300)
+                .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+            
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
