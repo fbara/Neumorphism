48db38fbf72c6799bdb62bba73dc360db84a5835
Frank S. Bara <fbara1@yahoo.com>
2020-02-29 23:30:54 -0600

    Add colorful accessibility to the buttons. Final commit.

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -14,6 +14,9 @@ extension Color {
     
     static let darkStart = Color(red: 50/255, green: 60/255, blue: 65/255)
     static let darkEnd = Color(red: 25/255, green: 25/255, blue: 30/255)
+    
+    static let lightStart = Color(red: 60/255, green: 160/255, blue: 240/255)
+    static let lightEnd = Color(red: 30/255, green: 80/255, blue: 120/255)
 }
 
 extension LinearGradient {
@@ -25,8 +28,8 @@ extension LinearGradient {
 struct SimpleButtonStyle: ButtonStyle {
     func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
-        .padding(30)
-        .contentShape(Circle())
+            .padding(30)
+            .contentShape(Circle())
             .background(
                 Group {
                     if configuration.isPressed {
@@ -35,16 +38,16 @@ struct SimpleButtonStyle: ButtonStyle {
                             .overlay(
                                 Circle()
                                     .stroke(Color.gray, lineWidth: 4)
-                                .blur(radius: 4)
-                                .offset(x: 2, y: 2)
+                                    .blur(radius: 4)
+                                    .offset(x: 2, y: 2)
                                     .mask(Circle().fill(LinearGradient(Color.black, Color.clear)))
-                            )
-                        .overlay(
-                            Circle()
-                                .stroke(Color.white, lineWidth: 8)
-                            .blur(radius: 4)
-                            .offset(x: -2, y: -2)
-                                .mask(Circle().fill(LinearGradient(Color.clear, Color.black)))
+                        )
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.white, lineWidth: 8)
+                                    .blur(radius: 4)
+                                    .offset(x: -2, y: -2)
+                                    .mask(Circle().fill(LinearGradient(Color.clear, Color.black)))
                         )
                     } else {
                         Circle()
@@ -81,6 +84,30 @@ struct DarkBackground<S: Shape>: View {
     }
 }
 
+struct ColorfulBackground<S: Shape>: View {
+    var isHighlighted: Bool
+    var shape: S
+    
+    var body: some View {
+        ZStack {
+            if isHighlighted {
+                shape
+                    .fill(LinearGradient(Color.lightEnd, Color.lightStart))
+                    .overlay(shape.stroke(LinearGradient(Color.lightStart, Color.lightEnd), lineWidth: 4))
+                    .shadow(color: Color.darkStart, radius: 10, x: 5, y: 5)
+                    .shadow(color: Color.darkEnd, radius: 10, x: -5, y: -5)
+            } else {
+                shape
+                    .fill(LinearGradient(Color.darkStart, Color.darkEnd))
+                    .overlay(shape.stroke(LinearGradient(Color.lightStart, Color.lightEnd), lineWidth: 4))
+                    .shadow(color: Color.darkStart, radius: 10, x: -10, y: -10)
+                    .shadow(color: Color.darkEnd, radius: 10, x: 10, y: 10)
+                
+            }
+        }
+    }
+}
+
 struct DarkButtonStyle: ButtonStyle {
     func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
@@ -108,6 +135,33 @@ struct DarkToggleStyle: ToggleStyle {
     }
 }
 
+struct ColorfulButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+        .padding(30)
+        .contentShape(Circle())
+        .background(
+            ColorfulBackground(isHighlighted: configuration.isPressed, shape: Circle())
+        )
+        .animation(nil)
+    }
+}
+
+struct ColorfulToggleStyle: ToggleStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        Button(action: {
+            configuration.isOn.toggle()
+        }) {
+            configuration.label
+                .padding(30)
+                .contentShape(Circle())
+        }
+        .background(
+            ColorfulBackground(isHighlighted: configuration.isOn, shape: Circle())
+        )
+    }
+}
+
 struct ContentView: View {
     @State private var isToggled = false
     
@@ -130,13 +184,13 @@ struct ContentView: View {
                         .foregroundColor(.white)
                     
                 }
-                .buttonStyle(DarkButtonStyle())
+                .buttonStyle(ColorfulButtonStyle())
                 
                 Toggle(isOn: $isToggled) {
                     Image(systemName: "heart.fill")
                         .foregroundColor(.white)
                 }
-            .toggleStyle(DarkToggleStyle())
+            .toggleStyle(ColorfulToggleStyle())
             }
             
         }
